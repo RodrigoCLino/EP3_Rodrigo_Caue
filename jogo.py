f4b81293c34b00a39aa28a6076f64c14ecfeceef
RodrigoCLino <digo.lino@hotmail.com>
2016-04-26 08:09:26 -0300

    nova função verificar vencedor

--- jogo.py
+++ jogo.py
@@ -18,20 +18,22 @@ class Jogo:
        self.turn += 1
        
    def verifica_ganhador(self,jogador):
-       if self.matriz_geral[0][0] == jogador and self.matriz_geral[0][1] == jogador and self.matriz_geral[0][2] == jogador:
-           return self.label_turno.configure(text="Vitoria {0}" .format(jogador))
-       elif self.matriz_geral[1][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[1][2] == jogador:
-           return 1
-       elif self.matriz_geral[2][0] == jogador and self.matriz_geral[2][1] == jogador and self.matriz_geral[2][2] == jogador:
-           return 1
-       elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][0] == jogador and self.matriz_geral[2][0] == jogador:
-           return 1
-       elif self.matriz_geral[0][1] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][1] == jogador:
-           return 1
-       elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][2] == jogador and self.matriz_geral[2][2] == jogador:
-           return 1
-       elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][0] == jogador:
-           return 1
+        if self.matriz_geral[0][0] == jogador and self.matriz_geral[0][1] == jogador and self.matriz_geral[0][2] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[1][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[1][2] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[2][0] == jogador and self.matriz_geral[2][1] == jogador and self.matriz_geral[2][2] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][0] == jogador and self.matriz_geral[2][0] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[0][1] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][1] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][2] == jogador and self.matriz_geral[2][2] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][2] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
+        elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][0] == jogador:
+            return self.label_turno.configure(text="Vitoria Jogador {0}" .format(jogador))
    
    #def limpa_jogadas():
 
