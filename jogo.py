a256f30b6faa32f926eed6d267dfc1f3097b0dfc
RodrigoCLino <digo.lino@hotmail.com>
2016-04-26 14:47:11 -0300

    arrumei classe verifica ganhador

--- jogo.py
+++ jogo.py
@@ -11,55 +11,48 @@ class Jogo:
    def recebe_jogada(self,linha,coluna):
        if self.turn == 1:
           self.matriz_geral[linha][coluna]="X"
-          self.verifica_ganhador("X")
+          self.turn = 2
           print(self.matriz_geral)
-          print(self.lista_jogadas)
-          self.turn==2
-       elif self.turn==2:
+       elif self.turn == 2:
           self.matriz_geral[linha][coluna]="O"
-          self.verifica_ganhador("O")
+          self.turn = 1
           print(self.matriz_geral)
-          print(self.lista_jogadas)
-          self.turn==1
-       
-       
-   def verifica_ganhador(self,jogador):
-        self.lista_jogadas.append(jogador)
-        if jogador == "X":
-            if self.matriz_geral[0][0] == jogador and self.matriz_geral[0][1] == jogador and self.matriz_geral[0][2] == jogador:
+          
+   def verifica_ganhador(self):
+        
+        if self.matriz_geral[0][0] =="X" and self.matriz_geral[0][1] == "X" and self.matriz_geral[0][2] == "X ":
                 return 1
-            elif self.matriz_geral[1][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[1][2] == jogador:
+        elif self.matriz_geral[0][0] =="O" and self.matriz_geral[0][1] == "O" and self.matriz_geral[0][2] == "O":
+                return 2
+        elif self.matriz_geral[1][0] == "X" and self.matriz_geral[1][1] == "X" and self.matriz_geral[1][2] == "X":            
                 return 1
-            elif self.matriz_geral[2][0] == jogador and self.matriz_geral[2][1] == jogador and self.matriz_geral[2][2] == jogador:
+        elif self.matriz_geral[1][0] == "O" and self.matriz_geral[1][1] == "O" and self.matriz_geral[1][2] == "O": 
+                return 2
+        elif self.matriz_geral[2][0] == "X" and self.matriz_geral[2][1] == "X" and self.matriz_geral[2][2] == "X":            
                 return 1
-            elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][0] == jogador and self.matriz_geral[2][0] == jogador:
+        elif self.matriz_geral[2][0] == "O" and self.matriz_geral[2][1] == "O" and self.matriz_geral[2][2] == "O":  
+                return 2
+        elif self.matriz_geral[0][0] == "X" and self.matriz_geral[1][0] == "X" and self.matriz_geral[2][0] == "X":
                 return 1
-            elif self.matriz_geral[0][1] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][1] == jogador:
+        elif self.matriz_geral[0][0] == "O" and self.matriz_geral[1][0] == "O" and self.matriz_geral[2][0] == "O":
+                return 2
+        elif self.matriz_geral[0][1] == "X" and self.matriz_geral[1][1] == "X" and self.matriz_geral[2][1] == "X":
                 return 1
-            elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][2] == jogador and self.matriz_geral[2][2] == jogador:
+        elif self.matriz_geral[0][1] == "O" and self.matriz_geral[1][1] == "O" and self.matriz_geral[2][1] == "O":
+                return 2
+        elif self.matriz_geral[0][2] == "X" and self.matriz_geral[1][2] == "X" and self.matriz_geral[2][2] == "X":
                 return 1
-            elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][2] == jogador:
+        elif self.matriz_geral[0][2] == "O" and self.matriz_geral[1][2] == "O" and self.matriz_geral[2][2] == "O":
+                return 2
+        elif self.matriz_geral[0][0] == "X" and self.matriz_geral[1][1] == "X" and self.matriz_geral[2][2] == "X":
                 return 1
-            elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][0] == jogador:
+        elif self.matriz_geral[0][0] == "O" and self.matriz_geral[1][1] == "O" and self.matriz_geral[2][2] == "O":
+                return 2
+        elif self.matriz_geral[0][2] == "X" and self.matriz_geral[1][1] == "X" and self.matriz_geral[2][0] == "X":
                 return 1
-        elif jogador == "O":
-            if self.matriz_geral[0][0] == jogador and self.matriz_geral[0][1] == jogador and self.matriz_geral[0][2] == jogador:
-                return 2
-            elif self.matriz_geral[1][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[1][2] == jogador:
-                return 2
-            elif self.matriz_geral[2][0] == jogador and self.matriz_geral[2][1] == jogador and self.matriz_geral[2][2] == jogador:
-                return 2
-            elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][0] == jogador and self.matriz_geral[2][0] == jogador:
-                return 2
-            elif self.matriz_geral[0][1] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][1] == jogador:
-                return 2
-            elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][2] == jogador and self.matriz_geral[2][2] == jogador:
-                return 2
-            elif self.matriz_geral[0][0] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][2] == jogador:
-                return 2
-            elif self.matriz_geral[0][2] == jogador and self.matriz_geral[1][1] == jogador and self.matriz_geral[2][0] == jogador:
-                return 2
-        elif len(self.lista_jogadas) == 9:
-            return -1
+        elif self.matriz_geral[0][2] == "O" and self.matriz_geral[1][1] == "O" and self.matriz_geral[2][0] == "O":
+                return 2 
+        elif self.matriz_geral[0][0]!=0 and self.matriz_geral[0][1]!=0 and self.matriz_geral[0][2]!=0 and self.matriz_geral[1][0]!=0 and self.matriz_geral[1][1]!=0 and self.matriz_geral[1][2]!=0 and self.matriz_geral[2][0]!=0 and self.matriz_geral[2][1]!=0 and self.matriz_geral[2][2]!=0:
+            return 0
             
    #def limpa_jogadas():
